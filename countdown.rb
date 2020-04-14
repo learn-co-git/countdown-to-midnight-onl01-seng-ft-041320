2f8e0edd65f4c5fa8eed77cb24eb92add9a28c3b
Neil Empey <argus.two.2@gmail.com>
2020-04-14 16:43:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,17 @@ def countdown(n)
   return "HAPPY NEW YEAR!"
 end
 countdown(10)
+
+def countdown_with_sleep(n)
+	while n > 0 do
+		print "#{n} SECOND(S)!\n"
+		n -= 1
+		y = 0
+		while y <= 38000000 do 
+			y += 1
+		end
+	end
+	return "HAPPY NEW YEAR!"
+end
+
+countdown_with_sleep(10)
